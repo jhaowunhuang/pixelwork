1a53392f662302280c9bd734266b2dff534f9243
jhaowunhuang <jhaowunhuang@gmail.com>
2020-03-30 01:34:38 +0800

    make some shapes

--- image_creat.py
+++ image_creat.py
@@ -1,18 +1,34 @@
 import cv2
 import numpy as np
 
-def func():
-    radius = 512
-    position = [256, 256]
-    signals = int(1000000)
-    cov = [[radius, 0], [0, radius]]
-    x, y = np.random.multivariate_normal(position, cov, signals).T
-    xedges = range(512)
-    yedges = range(512)
-    H, xedges, yedges = np.histogram2d(x, y, bins = (xedges, yedges))
-    H = H.T
-    print(H.max())
-    print(H.min())
-    cv2.imwrite('test.jpg', H)
+class pixelwork:
+    def __init__(self):
+        self.pix_map = {}
+        self.img_size = 512
+        self.sigma = 8 
+        self.cov = [[self.sigma, 0], [0, self.sigma]]
+        self.signals = 100
 
-func()
+    def add_pixel(self, x, y):
+        self.pix_map[(x, y)] = np.random.multivariate_normal([x, y], self.cov, self.signals).T
+
+    def draw(self):
+        x_sum = np.concatenate([self.pix_map[item][1] for item in self.pix_map])
+        y_sum = np.concatenate([self.pix_map[item][0] for item in self.pix_map])
+        xedges = range(self.img_size)
+        yedges = range(self.img_size)
+        H, xedges, yedges = np.histogram2d(x_sum, y_sum, bins = (xedges, yedges))
+        print(H.max())
+        cv2.imwrite('test.jpg', H)
+
+new_pic = pixelwork()
+for x in range(100, 250):
+    for y in range(50, 200):
+        new_pic.add_pixel(x, y)
+
+for x in range(512):
+    for y in range(512):
+        if (x - 350) * (x - 350) + (y - 250) * (y - 250) <= 2500:
+            new_pic.add_pixel(x, y)
+
+new_pic.draw()
